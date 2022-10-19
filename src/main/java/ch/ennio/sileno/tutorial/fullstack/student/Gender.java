51037253ce32944822bacc65dc69f95457a2a221
Ennio Sileno <silenenn@students.zhaw.ch>
2022-10-19 18:01:40 +0200

    added Student model

---
+++ src/main/java/ch/ennio/sileno/tutorial/fullstack/student/Gender.java
@@ -0,0 +1,7 @@
+package ch.ennio.sileno.tutorial.fullstack.student;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ src/main/java/ch/ennio/sileno/tutorial/fullstack/student/Student.java
@@ -0,0 +1,16 @@
+package ch.ennio.sileno.tutorial.fullstack.student;
+
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private Gender gender;
+}
