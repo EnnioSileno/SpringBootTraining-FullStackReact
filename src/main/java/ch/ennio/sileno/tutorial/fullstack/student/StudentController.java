d090db903f12e322011d1f6151deeaad1e844414
Ennio Sileno <silenenn@students.zhaw.ch>
2022-10-23 19:26:31 +0200

    created StudentService and StudentRepository

--- src/main/java/ch/ennio/sileno/tutorial/fullstack/student/StudentController.java
+++ src/main/java/ch/ennio/sileno/tutorial/fullstack/student/StudentController.java
@@ -1,30 +1,21 @@
 package ch.ennio.sileno.tutorial.fullstack.student;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/students")
+@AllArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
 
     @GetMapping
     public List<Student> getAllStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Ennio",
-                        "e@gmail.com",
-                        Gender.MALE),
-                new Student(
-                        2L,
-                        "Manu",
-                        "m@gmail.com",
-                        Gender.MALE
-                )
-        );
+        return studentService.getAllStudents();
     }
 }
