caf7f3df6bde9e2b66d1c47afbc9708f49a9acea
Ennio Sileno <silenenn@students.zhaw.ch>
2022-10-25 12:04:17 +0200

    created radio button group

--- src/frontend/src/components/buttonGroupDeleteEdit.tsx
+++ src/frontend/src/components/buttonGroupDeleteEdit.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import { Radio } from 'antd';
 
 
-const ButtonGroupDeleteEdit = (): JSX.Element => {
+const ButtonGroupDeleteEdit: React.FC = (): JSX.Element => {
     return (
-    <>
-        Test
-    </>);
+        <Radio.Group onChange={(e) => console.log(e + ' was pressed')}>
+            <Radio.Button onClick={() => console.log('Delete student clicked')}>Delete</Radio.Button>
+            <Radio.Button onClick={() => console.log('Edit student clicked')}>Edit</Radio.Button>
+        </Radio.Group>
+    );
 }
 
 export default ButtonGroupDeleteEdit;
