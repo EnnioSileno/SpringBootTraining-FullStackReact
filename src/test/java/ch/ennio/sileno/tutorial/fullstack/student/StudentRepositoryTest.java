7595ef8784bb932c7c1b2e6b96d38231c21b6279
Ennio Sileno <silenenn@students.zhaw.ch>
2022-11-04 15:17:30 +0100

    StudentRepository tests created

--- src/test/java/ch/ennio/sileno/tutorial/fullstack/student/StudentRepositoryTest.java
+++ src/test/java/ch/ennio/sileno/tutorial/fullstack/student/StudentRepositoryTest.java
@@ -1,17 +1,25 @@
 package ch.ennio.sileno.tutorial.fullstack.student;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class StudentRepositoryTest {
 
     @Autowired
     private StudentRepository underTest;
 
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
+
     @Test
-    void shouldReturnTrue_WhenSelectExistsEmail_GivenStudentExists() {
+    void shouldReturnTrue_WhenSelectExistsEmail_GivenStudentExistsInDB() {
         // given
         String email = "j@gmail.com";
         Student student = new Student(
@@ -26,5 +34,17 @@ class StudentRepositoryTest {
 
         // then
         assertTrue(studentExists);
+    }
+
+    @Test
+    void shouldReturnFalse_WhenSelectExistsEmail_GivenStudentNotExistsInDB() {
+        // given
+        String email = "j@gmail.com";
+
+        // when
+        boolean studentExists = underTest.selectExistsEmail(email);
+
+        // then
+        assertFalse(studentExists);
     }
 }
