6d7ac1809cccfe1f57adbc8411de4592ff903248
Ennio Sileno <silenenn@students.zhaw.ch>
2022-11-04 17:57:42 +0100

    created integreation test for deleteStudent

--- src/test/java/ch/ennio/sileno/tutorial/fullstack/integration/StudentIT.java
+++ src/test/java/ch/ennio/sileno/tutorial/fullstack/integration/StudentIT.java
@@ -3,6 +3,7 @@ package ch.ennio.sileno.tutorial.fullstack.integration;
 import ch.ennio.sileno.tutorial.fullstack.student.Gender;
 import ch.ennio.sileno.tutorial.fullstack.student.Student;
 import ch.ennio.sileno.tutorial.fullstack.student.StudentRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.util.StringUtils;
 
@@ -19,7 +21,8 @@ import org.springframework.util.StringUtils;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -61,5 +64,61 @@ public class StudentIT {
         assertThat(students)
                 .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
                 .contains(student);
+    }
+
+    @Test
+    void canDeleteStudent() throws Exception {
+        // given
+        String name = String.format(
+                "%s %s",
+                faker.name().firstName(),
+                faker.name().lastName()
+        );
+
+        String email = String.format("%s@amigoscode.edu",
+                StringUtils.trimAllWhitespace(name.trim().toLowerCase()));
+
+        Student student = new Student(
+                name,
+                email,
+                Gender.FEMALE
+        );
+
+        mockMvc.perform(post("/api/v1/students")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(student)))
+                .andExpect(status().isOk());
+
+        MvcResult getStudentsResult = mockMvc.perform(get("/api/v1/students")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String contentAsString = getStudentsResult
+                .getResponse()
+                .getContentAsString();
+
+        List<Student> students = objectMapper.readValue(
+                contentAsString,
+                new TypeReference<>() {
+                }
+        );
+
+        long id = students.stream()
+                .filter(s -> s.getEmail().equals(student.getEmail()))
+                .map(Student::getId)
+                .findFirst()
+                .orElseThrow(() ->
+                        new IllegalStateException(
+                                "student with email: " + email + " not found"));
+
+        // when
+        ResultActions resultActions = mockMvc
+                .perform(delete("/api/v1/students/" + id));
+
+        // then
+        resultActions.andExpect(status().isOk());
+        boolean exists = studentRepository.existsById(id);
+        assertFalse(exists);
     }
 }
